d56ec98ae91908b909132005b45ab1fb5cc366cb
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-05-28 13:52:46 +0200

    built initial reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,19 @@
+
+const initialState = {
+  count: 0
+}
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INCREASE':
+      return {
+        count: state.count + 1
+      }
+    case 'DECREASE':
+      return {
+        count: state.count - 1
+      }
+    default:
+      return state;
+  }
+}
